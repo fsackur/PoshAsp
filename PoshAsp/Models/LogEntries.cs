189ec0e6cd5b55f36202de28769adbf8aba1ba4d
tombotch <thomas.bottrill@rackspace.co.uk>
2015-03-05 17:08:13 +0000

    Added LogEntries

---
+++ PoshAsp/Models/LogEntries.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using MongoDB.Bson;
+using MongoDB.Driver;
+using MongoDB.Driver.Builders;
+
+namespace PoshAsp.Models
+{
+    public class LogEntries : List<LogEntry>
+    {
+        public LogEntries()
+        {
+
+        }
+
+        public LogEntries(string LogName)
+        {
+            MongoClient client = new MongoClient(Properties.Settings.Default.MongoConnectionString);
+            MongoServer server = client.GetServer();
+            MongoDatabase db = server.GetDatabase(Properties.Settings.Default.MongoDb);
+            MongoCollection<BsonDocument> entries = db.GetCollection<BsonDocument>(LogName);
+
+            foreach (LogEntry entry in entries.FindAllAs<LogEntry>())
+            {
+                this.Add(entry);
+            }
+        }
+    }
+}
